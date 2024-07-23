8a9ed5fd8172f47751824547dcb70a6185dd657e
mnreggi <matutereggi@hotmail.com>
2024-07-23 15:20:12 +1200

    I have no idea

---
+++ LinqEntityFramework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinqEntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
